abf511180aba84a44e7ffae0568022d1852148a5
Shivraj <shivrajganacharya1@gmail.com>
2021-03-10 16:20:29 +0530

    first commit

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState, useRef} from 'react'
-import '../App.css'
+import '../App.css' 
 import CalculatorService from '../services/CalculatorService'
 
 function Calculator() {
